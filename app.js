c4ae0960ee66c5ec088c55b041f00cccf1e78d65
Sania Mirza <saniamirza2094@gmail.com>
2025-12-18 18:03:40 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// ____________ var,let ,const variables
 var a = "sania";  // don`t give error on same name (a)
 var a = "mirza";
 console.log(a);  // mirza
@@ -12,23 +13,23 @@ console.log(fName, lName); //sania mirza
 
 
 // const myName;  declarations must be initialized, 
-const myName = "sania" 
+const myName = "sania"
 // myName = "mirza" , can`t assign/reinitialized new value to constant value 
 console.log(myName);
 
-// Template literals:
+// ____________ Template literals:
 var egLitrl = `Hey , ${fName} ; its an example of a literal template, 
  text with bectics !!!`
 console.log(egLitrl);
 
-//1- _________Spread Operators makes shallow(reference) copies
+// ____________ Spread Operators makes shallow(reference) copies
 
-let arr1 = ["a", "b"]
+var arr1 = ["a", "b"]
 let cloneArr = [...arr1]
 console.log(cloneArr);  // ['a', 'b']
 
 //2- can also do merging
-let obj1 = {
+var obj1 = {
     myName: "Sania",
     institute: "SMIT"
 }
@@ -52,15 +53,14 @@ function sum(a, b, c, d, e) {
 console.log(sum(...numbers)) // 15
 
 
-
-// _________Rest Operators (Packed items):
+// ____________ Rest Operators (Packed items):
 
 // 1- In Arrays:
 let arr = ["apple", "mango", "banana", "cherry", "orange", "melon"];
 console.log(arr[3]); //cherry (In this method, We have to give reference so many times:bad practise )
 
 // That`s why we use "destructuring & rest operators":
-let arr1 = ["apple", "mango", "banana", "cherry", "orange", "melon"];
+let arr0 = ["apple", "mango", "banana", "cherry", "orange", "melon"];
 var [a, b, c, d, e, f] = arr1;   // destructuring
 console.log(b); //mango
 
@@ -80,15 +80,74 @@ console.log(obj.course); //WMA (In this method, We have to give reference so man
 
 //"destructuring & rest operators" also used in objects like this:
 
-let obj1 = {
+let obj0 = {
     fName: "sania",
     course: "WMA",
     timings: 2 - 4
 }
-let { fName, course, timings } = obj1  // destructuring (keys name likhenge apni mrzi se name nh denge arrays ki trhn)
-console.log(obj1.fName); //sania (appears like variables)
+let { course, timings } = obj1  // destructuring (keys name likhenge apni mrzi se name nh denge arrays ki trhn)
+console.log(obj0.fName); //sania (appears like variables)
+
+// ____________ Enhanced Object literals
+
+let keyName = "Fname";
+let accessToken = "sjadb784r3nr2387"
+let data = {
+    [keyName]: "Sania",
+    fullName() {
+        console.log("Sania");
+    },
+    accessToken
+}
+console.log(data);
 
 
+// ____________ Default parameter
+function product(a = 0, b = 0) {
+    console.log(a * b);
+
+}
+product(3, 3) //9
+product() //15
+
+// ____________ Arrow Functions
+let greet = user => "Welcome" + " " + user
+let greet1 = () => "Welcome" + " " + "Hard coded name"
+
+console.log(greet("Sania"));
+console.log(greet1());
+
+let square = num => num * num
+console.log(square(45));
 
 
+let subtract = (num1, num2) => {
+    let result = num1 - num2
+    return result
+}
+console.log(subtract(7, 4));
 
+// ___________ Classes
+class AddmissionForm {
+    constructor(name, grade) {
+        this.name = name
+        this.grade = grade
+    }
+    cancel() {
+        console.log(`This form was cancelled by ${this.name}`);
+    }
+    submit() {
+        console.log(`This form was submitted by ${this.name}`);
+    }
+    // fill(name, grade) {
+    //     this.name = name
+    //     this.grade = grade
+    // }
+}
+let sania = new AddmissionForm("Sania", 12)
+let neha = new AddmissionForm("Neha", 8)
+console.log(sania);
+// sania.fill("Sania", 12)
+sania.cancel()
+// neha.fill("Neha", 8)
+neha.submit()
